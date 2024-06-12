9950a83cfcafb6c2cef681cd243ef64e2dfac3c6
Bruno Vitalino <brunovitalino@hotmail.com>
2024-06-12 12:10:26 -0300

    Java project created.

---
+++ src/main/java/lol/bvlabs/yessir/YesSirApplication.java
@@ -0,0 +1,13 @@
+package lol.bvlabs.yessir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesSirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesSirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lol/bvlabs/yessir/YesSirApplicationTests.java
@@ -0,0 +1,13 @@
+package lol.bvlabs.yessir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YesSirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
