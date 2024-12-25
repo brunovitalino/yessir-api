a825056f3f64a08a6650fb1ea91ebebf000564d0
Bruno Vitalino <brunovitalino@hotmail.com>
2024-12-25 12:33:29 -0300

    Adicionada variaveis para metricas de erros e sucessos de logins

--- src/main/java/lol/bvlabs/yessir/controller/AutenticacaoController.java
+++ src/main/java/lol/bvlabs/yessir/controller/AutenticacaoController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lol.bvlabs.yessir.domain.usuario.DadosAutenticacao;
 import lol.bvlabs.yessir.domain.usuario.Usuario;
 import lol.bvlabs.yessir.infra.security.DadosJWTToken;
@@ -17,6 +19,19 @@ import lol.bvlabs.yessir.infra.security.TokenService;
 @RestController
 @RequestMapping("/auth/login")
 public class AutenticacaoController {
+
+	Counter authUserSuccess;
+	Counter authUserError;
+	
+	public AutenticacaoController(MeterRegistry registry) {
+		authUserSuccess = Counter.builder("auth_user_success")
+				.description("usuarios autenticados")
+				.register(registry);
+
+		authUserError = Counter.builder("auth_user_error")
+				.description("erros de login")
+				.register(registry);
+	}
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
@@ -26,10 +41,18 @@ public class AutenticacaoController {
 	
 	@PostMapping
 	public ResponseEntity<DadosJWTToken> efetuarLogin(@RequestBody DadosAutenticacao dados) {
-		var tokenAuthentication = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
-		var authentication = authenticationManager.authenticate(tokenAuthentication);
-		var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
-		return ResponseEntity.ok(new DadosJWTToken(tokenJWT));
+		var loginInfo = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+
+		try {
+			var authentication = authenticationManager.authenticate(loginInfo);
+			var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+			authUserSuccess.increment();
+			return ResponseEntity.ok(new DadosJWTToken(tokenJWT));
+
+		} catch (Exception e) {
+			authUserError.increment();
+			return ResponseEntity.badRequest().build();
+		}
 	}
 
 }
