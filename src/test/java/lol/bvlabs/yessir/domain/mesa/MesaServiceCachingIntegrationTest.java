8f5256fbb7e89ca54ab675b0e6e6564e9f2269df
Bruno Vitalino <brunovitalino@hotmail.com>
2025-03-09 19:55:37 -0300

    Adicionado teste de cache get

--- src/test/java/lol/bvlabs/yessir/domain/mesa/MesaServiceCachingIntegrationTest.java
+++ src/test/java/lol/bvlabs/yessir/domain/mesa/MesaServiceCachingIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheConfig;
@@ -22,13 +23,14 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@Import({ MesaService.class})
-@ExtendWith(SpringExtension.class)
-@EnableCaching
-@ImportAutoConfiguration(classes = { 
+//@Import({ MesaService.class})
+//@ExtendWith(SpringExtension.class)
+//@EnableCaching
+/*@ImportAutoConfiguration(classes = { 
   CacheAutoConfiguration.class, 
   RedisAutoConfiguration.class 
-})
+})*/
+@SpringBootTest
 @ActiveProfiles("test")
 class MesaServiceCachingIntegrationTest {
 
@@ -46,17 +48,18 @@ class MesaServiceCachingIntegrationTest {
     	var id = 1L;
     	var nome = "mesa 1";
         var mesa = new Mesa(id, nome);
-        var mesaOptional = new DadosListagemMesa(mesa);
+        var mesaOptional = Optional.of(new DadosListagemMesa(mesa));
+
         given(mockMesaRepository.findById(id))
           .willReturn(Optional.of(mesa));
 
-        var itemCacheMiss = mesaService.getMesaForId(id);
-        var itemCacheHit = mesaService.getMesaForId(id);
+        cacheManager.getCache("mesas").evict("mesa:1");
+        var mesaCacheMiss = mesaService.getOneById(id);
+        var mesaCacheHit = mesaService.getOneById(id);
 
-        assertThat(itemCacheMiss).isEqualTo(mesaOptional);
-        assertThat(itemCacheHit).isEqualTo(mesaOptional);
+        assertThat(mesaCacheMiss).isEqualTo(mesaOptional);
+        assertThat(mesaCacheHit).isEqualTo(mesaOptional);
 
         verify(mockMesaRepository, times(1)).findById(id);
-        //assertThat(itemFromCache()).isEqualTo(anItem);
     }
 }
